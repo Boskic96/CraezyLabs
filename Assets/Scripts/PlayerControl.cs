f40c468de9b8cf1508e6b971c33e703c2e2278e3
Boskic96 <62135027+Boskic96@users.noreply.github.com>
2021-04-22 11:05:07 +0200

    touch input

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,9 +7,26 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private Vector3 touchPosition;
+
+    private void OnEnable()
+    {
+        TouchControls.PlayerInput += MovePlayer;
+    }
+
+    private void OnDisable()
+    {
+        TouchControls.PlayerInput -= MovePlayer;
+    }
+
     private void FixedUpdate()
     {
         var movingTarget = transform.position + transform.forward;
         transform.position = Vector3.Lerp(transform.position, movingTarget, speed);
+    }
+
+    private void MovePlayer(Vector3 touchPosition)
+    {
+        this.touchPosition = touchPosition;
     }
 }
--- Assets/Scripts/TouchControls.cs
+++ Assets/Scripts/TouchControls.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class TouchControls : MonoBehaviour
 {
+    public delegate void ControllsInput(Vector3 position);
+    public static ControllsInput PlayerInput;
+
+    private bool isFirstTouch = true;
+
+    private Vector2 firstTouch;
+
     private void Update()
     {
         if(Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0;
+            if (isFirstTouch)
+            {
+                firstTouch = touch.position;
+                isFirstTouch = false;
+            }
+
+            var currentTouch = touch.position;
+            var returnVector = currentTouch - firstTouch;
+
+            Debug.Log(returnVector);
+            PlayerInput(returnVector);
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                isFirstTouch = true;
+            }
         }
     }
 }
